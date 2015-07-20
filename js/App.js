6b4e2fea00ec676074bf713a8f436ca9c9301cc9
Guilherme Teodoro <guilherme.teodoro@apontador.com>
2015-07-20 15:29:46 -0300

    Fix indentation

--- js/App.js
+++ js/App.js
@@ -6,15 +6,16 @@ var App = React.createClass({
   },
   handleChange: function (e) {
     this.setState({text: e.target.value});
-  },  
-	render: function() {
-		return (
-			<div>
+  },
+  render: function() {
+    return (
+      <div>
+        <h1>React Blank</h1>
         <input type="text" onChange={this.handleChange}/>
         {this.state.text}
-  		</div>
-		);
-	}
+      </div>
+    );
+  }
 });
 
 module.exports = App;
